992e3b51f5667b61d851dc014e6ff265f3521f07
Ryan Parlin <parlinr@ad.nmc.edu>
2018-04-05 12:13:06 -0400

    DST seed data added

--- setup.sql
+++ setup.sql
@@ -9,8 +9,6 @@ CREATE TABLE quarterback (
     firstName VARCHAR(50),
     year INT,
     week INT,
-    completions INT,
-    attempts INT,
     touchdowns INT,
     interceptions INT,
     yards INT
@@ -23,7 +21,6 @@ CREATE TABLE runningBack (
     firstName VARCHAR(50),
     year INT,
     week INT,
-    carries INT,
     yards INT,
     touchdowns INT
 );
@@ -74,6 +71,32 @@ ALTER TABLE wideReceiver AUTO_INCREMENT=3000;
 ALTER TABLE tightEnd AUTO_INCREMENT=4000;
 ALTER TABLE defenseSpecialTeams AUTO_INCREMENT=5000;
 
--- seed data 
+-- insert seed data 
+INSERT INTO quarterback ('lastName', 'firstName', 'year', 'week',
+'touchdowns','interceptions','yards') VALUES ();
 
+INSERT INTO runningBack ('lastName','firstName','year','week','yards','touchdowns')
+VALUES ();
+
+INSERT INTO wideReceiver ('lastName','firstName','year','week','receptions','yards','touchdowns')
+VALUES ();
+
+INSERT INTO tightEnd ('lastName','firstName','year','week','receptions','yards','touchdowns')
+VALUES ();
+
+INSERT INTO defenseSpecialTeams ('team','year','week','sacks','interceptions','fumbleRecoveries',
+'safeties','blockedKicks','conversionOrExtraPointReturns','pointsAllowed','returnsForTouchdown')
+VALUES ('New England Patriots','2017','13','4','1','0','0','0','0','3','0'),
+('Minnesota Vikings','2017','13','0','0','0','0','0','0','9','0'),('Jacksonville Jaguars','2017','13','4',
+'2','0','0','0','10','0'),('Philadelphia Eagles','2017','13','2','0','0','0','0','0','24','0'),
+('Minnesota Vikings','2017','14','2','1','0','0','0','0','31','0'),('Philadelphia Eagles','2017','14','2',
+'0','2','0','0','0','35','0'),('Jacksonville Jaguars','2017','14','2','3','0','0','0','0','24','0'),
+('Minnesota Vikings','2017','15','3','2','0','0','0','0','7','1'),('Philadelphia Eagles','2017','15','1','1','0',
+'0','0','0','29','0'),('Jacksonville Jaguars','2017','15','4','1','0','0','0','0','7','0'),
+('New England Patriots','2017','15','2','1','0','0','0','0','24','0'),('New England Patriots','2017','14','2','0','0','0',
+'0','0','27','0'),('Minnesota Vikings','2017','16','1','2','0','0','0','0','0','0'),('New England Patriots','2017','16',
+'6','0','0','0','0','0','16','0'),('Jacksonville Jaguars','2017','16','1','1','0','0','0','0','44','0'),
+('Philadelphia Eagles','2017','16','1','2','3','0','0','0','10','1'),('Minnesota Vikings','2017','17','1','0','0',
+'0','0','0','10','0'),('New England Patriots','2017','17','4','0','0','0','0','0','6','0'),('Philadelphia Eagles','2017','17',
+'1','0','1','0','0','0','6','0'),('Jacksonville Jaguars','2017','17','3','0','1','0','0','0','15','1');
 
